95c833d2ae9bc385ae46ab04167e7ba0ad272e55
Orji Joseph <orjiace@gmail.com>
2021-07-12 17:49:13 +0100

    second important commit

--- data/Blogs.ts
+++ data/Blogs.ts
@@ -3,7 +3,7 @@ export const Blogs = [
         id: '1',
         title: 'React native Biometrics the easy way',
         link: 'https://orjiace.medium.com/react-native-biometrics-the-easy-way-fc51aa992dff',
-        image: 'https://images.unsplash.com/photo-1611330500121-d9439ddc3d9d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80'
+        image: 'https://images.unsplash.com/photo-1616332012084-9ac36f0a7b3a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZmluZ2VycHJpbnR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
     },    {
         id: '2',
         title: 'RN Snaptchat bottom tab',
@@ -27,9 +27,9 @@ export const Projects = [
     },
     {
         id: '3',
-        projectName: 'Crowdfacture',
+        projectName: 'Fundible',
         image: '',
-        shortDescription: 'Crowdfacture is a Community driven production, manufacturing and distribution company by the people for the people',
+        shortDescription: 'A block chain crowdfunding app, that lets users raise funds for a project in Bitcoin & and also withdraw to any wallet address',
         links: [
             'https://Crowdfacture.com',
             'https://play.google.com/store/apps/details?id=com.crowdfacture.crowdfacture_mobile'
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,8 @@ import Button from "../components/Button";
 
 import TechStack from "../components/TechStack";
 import StackChain from "../components/StackChain";
-import { useSpring, animated, to } from '@react-spring/web'
-import { useGesture } from 'react-use-gesture'
-
-
+import {useSpring, animated, to} from '@react-spring/web'
+import {useGesture} from 'react-use-gesture'
 
 
 const calcX = (y: number, ly: number) => -(y - ly - window.innerHeight / 2) / 20
@@ -26,7 +24,6 @@ const Home = () => {
     const [offsetY, setOffsetY] = useState(0);
     const handleScroll = () => {
         setOffsetY(window.pageYOffset);
-        console.log(offsetY)
     }
 
     useEffect(() => {
@@ -48,9 +45,8 @@ const Home = () => {
     }, [])
 
 
-
     const domTarget = useRef(null)
-    const [{ x, y, rotateX, rotateY, rotateZ, zoom, scale }, api] = useSpring(
+    const [{x, y, rotateX, rotateY, rotateZ, zoom, scale}, api] = useSpring(
         () => ({
             rotateX: 0,
             rotateY: 0,
@@ -59,69 +55,64 @@ const Home = () => {
             zoom: 0,
             x: 0,
             y: 0,
-            config: { mass: 5, tension: 350, friction: 40 },
+            config: {mass: 5, tension: 350, friction: 40},
         })
     )
 
-    const [{ wheelY }, wheelApi] = useSpring(() => ({ wheelY: 0 }))
+    const [{wheelY}, wheelApi] = useSpring(() => ({wheelY: 0}))
     useGesture(
         {
-            onMove: ({ xy: [px, py], dragging }) =>
+            onMove: ({xy: [px, py], dragging}) =>
                 !dragging &&
                 api({
                     rotateX: calcX(py, y.get()),
                     rotateY: calcY(px, x.get()),
                     scale: 1.1,
                 }),
-            onHover: ({ hovering }) =>
-                !hovering && api({ rotateX: 0, rotateY: 0, scale: 1 }),
-            onWheel: ({ event, offset: [, y] }) => {
+            onHover: ({hovering}) =>
+                !hovering && api({rotateX: 0, rotateY: 0, scale: 1}),
+            onWheel: ({event, offset: [, y]}) => {
                 event.preventDefault()
-                wheelApi.set({ wheelY: y })
+                wheelApi.set({wheelY: y})
             },
         },
-        { domTarget, eventOptions: { passive: false } }
+        {domTarget, eventOptions: {passive: false}}
     )
-
-
 
 
     return (
         <div className={styles.home}>
 
-                <Banner offsetY={offsetY}/>
+            <Banner offsetY={offsetY}/>
 
 
-                <div className={styles.techStack}>
+            <div className={styles.techStack}>
 
 
-                <div className={styles.titleWrap}
-                     style={{ transform: `translateY(-${offsetY * 0.5}px)`, transition:'0.5s' }}>
+                <div className={styles.titleWrap} style={{transform: `translateY(-${offsetY * 0.5}px)`, transition: '0.5s'}}>
 
                     “Tech Stack .”
                 </div>
 
                 <div className={styles.stack}
-                     style={{ transform: `translateY(-${offsetY * 0.5}px)`, transition:'0.5s' }}>
+                     style={{transform: `translateY(-${offsetY * 0.5}px)`, transition: '0.5s'}}>
 
                     {/* array of JSX items */}
-                <TechStack/>
+                    <TechStack/>
 
                 </div>
 
 
-
-
-                </div>
-                <div
-                    className={styles.projectsContainer}
-                >
+            </div>
+            <div
+                className={styles.projectsContainer}
+            >
 
                 <section className={styles.pTitleWrap}>
                     Projects
                 </section>
 
-                <div className={styles.projectCardWrap} >
+                <div className={styles.projectCardWrap}>
                     {
                         Projects.map((project, index) => (
                             <ProjectCard key={project.id} projectDetails={project}/>
@@ -129,32 +120,30 @@ const Home = () => {
                         ))
                     }
                 </div>
-                </div>
-
-
+            </div>
 
 
             <div className={styles.blogs}>
-                <div>
+                <span>
                     Blogs
-                </div>
+                </span>
                 <div className={styles.bTitleWrap}
 
                 >
                     “
-                    Read my stories where i write my wrongs
+                    Stories and practical guids written by me
                     ”
                 </div>
 
                 <div className={styles.blogCardWrap}>
 
                     {
-                        Blogs.map((({title, id, image, link}) =>(
-
+                        Blogs.map((({title, id, image, link}) => (
+<div className={styles.blogCard}>
                             <animated.div
                                 key={id}
                                 ref={domTarget}
-                                className={styles.blogCard}
+                                className={styles.blogImg}
                                 style={{
                                     transform: 'perspective(400px)',
                                     x,
@@ -166,9 +155,15 @@ const Home = () => {
                                 }}>
 
 
-<img src={image} className={styles.blogImg} alt={title}/>
+                                <img src={image} className={styles.blogImg} alt={title}/>
+                            </animated.div>
+                                <div className={styles.blogTitle}>
+                                    {title}
+                                </div>
+                                <div className={styles.line}/>
 
-                                </animated.div>
+
+                            </div>
                         )))
                     }
 
